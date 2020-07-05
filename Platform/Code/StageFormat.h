f4b0a09fa11c552b7130254e24cc9197b4e538e7
Donya <alias>
2020-07-05 10:44:53 +0900

    ステージの書式を更新

--- Platform/Code/StageFormat.h
+++ Platform/Code/StageFormat.h
@@ -8,13 +8,16 @@ namespace StageFormat
 		Space			= 0,
 		StartPointRight	= 1,	// Looking right
 		StartPointLeft	= 2,	// Looking left
-		Normal			= 3,
-		ClearEvent		= 4,
+		ClearEvent		= 3,
+		Normal			= 4,
 
-		RoomStart		= 8,	// [RoomStart <= N <= RoomLast] is room identifier
-		RoomLast		= 15,	// [RoomStart <= N <= RoomLast] is room identifier
+		RoomStart		= 8,				// [RoomStart <= N <= RoomLast] is room identifier
+		RoomLast		= RoomStart + 15,	// [RoomStart <= N <= RoomLast] is room identifier
 
-		EnemyStart		= 16,	// [EnemyStart <= N <= EnemyLast] is room identifier
-		EnemyLast		= 23,	// [EnemyStart <= N <= EnemyLast] is room identifier
+		EnemyStart		= RoomLast + 1,		// [EnemyStart <= N <= EnemyLast] is enemy identifier
+		EnemyLast		= EnemyStart + 7,	// [EnemyStart <= N <= EnemyLast] is enemy identifier
+		
+		BossStart		= EnemyLast + 1,	// [BossStart <= N <= BossLast] is boss identifier
+		BossLast		= BossStart + 7,	// [BossStart <= N <= BossLast] is boss identifier
 	};
 }
