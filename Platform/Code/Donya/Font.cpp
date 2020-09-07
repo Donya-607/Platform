fd13be426d4145bde89276e226d43b4b7add83cc
Donya <alias>
2020-09-07 15:22:05 +0900

    ライブラリのフォント機構を追加

---
+++ Platform/Code/Donya/Font.cpp
@@ -0,0 +1,472 @@
+#include "Font.h"
+
+#include <algorithm>	// Use std::sort
+
+#include "Constant.h"	// Use scast macro
+#include "Donya.h"
+#include "Sprite.h"
+#include "Useful.h"		// Use some functions related to file path
+
+namespace Donya
+{
+	namespace Font
+	{
+		bool Holder::LoadFntFile( const std::wstring &filePath )
+		{
+			if ( !Donya::IsExistFile( filePath ) )
+			{
+				_ASSERT_EXPR( 0, L"Error: Passed .fnt file is not found." );
+				return false;
+			}
+			// else
+
+			const std::wstring fullPath = Donya::ToFullPath( filePath );
+
+			std::unique_ptr<char[]> pFntData = nullptr;
+			const long codeLength = Donya::ReadByteCode( &pFntData, fullPath );
+			if ( codeLength <= 0 || !pFntData )
+			{
+				_ASSERT_EXPR( 0, L"Failed: Read .fnt file is failed!" );
+				return false;
+			}
+			// else
+
+			constexpr const char *delimiters		= " ,=\"\n\r";
+			constexpr const char *delimitersNoSpace	= ",=\"\n\r";
+
+			char *pToken		= nullptr;
+			char *pNextToken	= nullptr;
+
+			pToken = strtok_s( pFntData.get(), delimiters, &pNextToken );
+			if ( strcmp( pToken, "info" ) != 0 )
+			{
+				_ASSERT_EXPR( 0, L"Error: This file does not match the .fnt format!" );
+				return false;
+			}
+			// else
+
+			auto ReadNextToken = [&pToken, &pNextToken]( const char *delimiters )
+			{
+				pToken = strtok_s( nullptr, delimiters, &pNextToken );
+			};
+
+		#pragma region information
+		
+			// face
+			ReadNextToken( delimiters );
+			ReadNextToken( delimitersNoSpace );
+			const char *face = pToken; // Used font name
+
+			// size
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int size = atoi( pToken );
+
+			// bold
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int bold = atoi( pToken );
+
+			// italic
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int italic = atoi( pToken );
+
+			// charset
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const char* charset = pToken;
+
+			// unicode
+			if ( strcmp( charset, "unicode" ) == 0 )
+			{ charset = ""; }
+			else
+			{ ReadNextToken( delimiters ); }
+			ReadNextToken( delimiters );
+			const int unicode = atoi( pToken );
+
+			// stretchH
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int stretchH = atoi( pToken );
+
+			// smooth
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int smooth = atoi( pToken );
+
+			// aa
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int aa = atoi( pToken );
+
+			// padding
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int padding1 = atoi( pToken );
+			ReadNextToken( delimiters );
+			const int padding2 = atoi( pToken );
+			ReadNextToken( delimiters );
+			const int padding3 = atoi( pToken );
+			ReadNextToken( delimiters );
+			const int padding4 = atoi( pToken );
+
+			// spacing
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int spacing0 = atoi( pToken );
+			ReadNextToken( delimiters );
+			const int spacing1 = atoi( pToken );
+
+			// outline
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int outline = atoi( pToken );
+
+		// region information
+		#pragma endregion
+
+		#pragma region common
+		
+			// common
+			ReadNextToken( delimiters );
+
+			// lineHeight
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int lineHeight = atoi( pToken );
+
+			// base
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int base = atoi( pToken );
+
+			// scaleW
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int scaleW = atoi( pToken );
+
+			// scaleH
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int scaleH = atoi( pToken );
+
+			// pages
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int pages = atoi( pToken );
+
+			// packed
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int packed = atoi( pToken );
+
+			// alphaChnl
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int alphaChnl = atoi( pToken );
+
+			// redChnl
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int redChnl = atoi( pToken );
+
+			// greenChnl
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int greenChnl = atoi( pToken );
+
+			// blueChnl
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int blueChnl = atoi( pToken );
+
+		// region common
+		#pragma endregion
+			
+		#pragma region page
+			
+			SetTextureDirectory( Donya::ExtractFileDirectoryFromFullPath( fullPath ) );
+			textureNames.resize( pages );
+			for ( int i = 0; i < pages; ++i )
+			{
+				// page
+				ReadNextToken( delimiters );
+
+				// id
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int id = atoi( pToken );
+
+				// file
+				ReadNextToken( delimiters );
+				ReadNextToken( delimitersNoSpace );
+				const char* file = pToken;
+
+				// textureNames[i] = Donya::MultiToWide( file );
+				textureNames[i] = Donya::UTF8ToWide( file );
+			}
+
+		// region page
+		#pragma endregion
+
+		#pragma region char
+
+			// chars
+			ReadNextToken( delimiters );
+
+			// count
+			ReadNextToken( delimiters );
+			ReadNextToken( delimiters );
+			const int count = atoi( pToken );
+
+
+			fontSize.x		= fabsf( scast<float>( size			) );
+			fontSize.y		= fabsf( scast<float>( lineHeight	) );
+			characterCount	= count + 1;
+			characters.resize( characterCount );
+			characterIndices.fill( 0 );
+			characterIndices[0x0000] = Character::Code::idEnd;
+			characterIndices[0x0009] = Character::Code::idTab;
+			characterIndices[0x000a] = Character::Code::idReturn;
+			characterIndices[0x0020] = Character::Code::idSpace;
+
+
+			int charIndex = 1;
+			for ( int i = 0; i < count; ++i )
+			{
+				// char
+				ReadNextToken( delimiters );
+
+				// id
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int id = atoi( pToken );
+
+				// x
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int x = atoi( pToken );
+
+				// y
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int y = atoi( pToken );
+
+				// width
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int width = atoi( pToken );
+
+				// height
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int height = atoi( pToken );
+
+				// xoffset
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int xoffset = atoi( pToken );
+
+				// yoffset
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int yoffset = atoi( pToken );
+
+				// xadvance
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int xadvance = atoi( pToken );
+
+				// page
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int page = atoi( pToken );
+
+				// chnl
+				ReadNextToken( delimiters );
+				ReadNextToken( delimiters );
+				const int chnl = atoi( pToken );
+
+				// Store the information if supported
+				if ( 0xFFFF < id ) { continue; }
+				// else
+
+				characterIndices[id] = scast<CharType>( i + 1 );
+
+				Character &element		= characters[charIndex];
+				element.uvMin.x			= scast<float>( x			) / scast<float>( scaleW );
+				element.uvMin.y			= scast<float>( y			) / scast<float>( scaleH );
+				element.uvMax.x			= scast<float>( x + width	) / scast<float>( scaleW );
+				element.uvMax.y			= scast<float>( y + height	) / scast<float>( scaleH );
+				element.uvPartSize.x	= scast<float>( width		);
+				element.uvPartSize.y	= scast<float>( height		);
+				element.offset.x		= scast<float>( xoffset		);
+				element.offset.y		= scast<float>( yoffset		);
+				element.advance			= scast<float>( xadvance	);
+				element.pageNo			= page;
+
+				charIndex++;
+			}
+
+		// region char
+		#pragma endregion
+
+			return true;
+		}
+		bool Holder::LoadByCereal( const std::string &filePath )
+		{
+			Donya::Serializer tmp;
+			return tmp.LoadBinary( *this, filePath.c_str(), serializeObjectName );
+		}
+		bool Holder::SaveByCereal( const std::string &filePath ) const
+		{
+			Donya::Serializer tmp;
+			return tmp.SaveBinary( *this, filePath.c_str(), serializeObjectName );
+		}
+		void Holder::SetTextureDirectory( const std::wstring &directory )
+		{
+			textureDirectory = directory;
+			if ( textureDirectory.back() != L'/' )
+			{
+				textureDirectory += L'/';
+			}
+		}
+
+
+		bool Renderer::Init( const Holder &fontData, size_t maxInstanceCount )
+		{
+			info = fontData;
+
+			const auto   &textureDir	= info.GetTextureDirectory();
+			const auto   &textureNames	= info.GetTextureNames();
+			const size_t textureCount	= textureNames.size();
+
+			std::wstring filePath;
+			textures.resize( textureCount );
+			for ( size_t i = 0; i < textureCount; ++i )
+			{
+				filePath = textureDir + textureNames[i];
+				if ( !Donya::IsExistFile( filePath ) )
+				{
+				#if DEBUG_MODE
+					std::wstring errMsg = L"Error: Font texture is not found!\n";
+					errMsg += L"Search directory: [";
+					errMsg += textureDir;
+					errMsg += L"]\n";
+					errMsg += L"Search fileName: [";
+					errMsg += textureNames[i];
+					errMsg += L"]";
+					Donya::ShowMessageBox( errMsg, L"File load is failed", MB_OK );
+				#endif // DEBUG_MODE
+					continue;
+				}
+				// else
+
+				textures[i].handle		= Donya::Sprite::Load( filePath, maxInstanceCount );
+				textures[i].wholeSize	= Donya::Sprite::GetTextureSizeF( textures[i].handle );
+			}
+
+
+			for ( const auto &it : textures )
+			{ if ( it.handle == NULL ) { return false; } }
+			// else
+			return true;
+		}
+		void Renderer::Draw( const wchar_t *string,			const Donya::Vector2 &ssPos, const Donya::Vector4 &color ) const
+		{
+			const auto &characters  = info.GetCharacters();
+			const auto &charIndices = info.GetCharacterIndices();
+			const Donya::Vector2 &origin	= ssPos;
+			const Donya::Vector2 &fontSize	= info.GetFontSize();
+			const size_t strLength			= wcslen( string );
+
+			struct Instance
+			{
+				size_t			handle = NULL;
+				Donya::Vector2	pos;	// Left-Top
+				Donya::Vector2	size;	// Whole size
+				Donya::Vector2	uvPos;	// Left-Top
+				Donya::Vector2	uvSize;	// Whole size
+			};
+			std::vector<Instance> elements{ strLength };
+
+			Donya::Vector2 drawPos = origin;
+
+			// Set drawing informations to "elements"
+			for ( size_t i = 0; i < strLength; ++i )
+			{
+				const CharType character = scast<CharType>( string[i] );
+				const CharType charCode  = charIndices[character];
+
+				if ( charCode == Character::Code::idEnd ) { break; }
+				// else
+				if ( charCode == Character::Code::idReturn )
+				{
+					drawPos.x =  origin.x;
+					drawPos.y += fontSize.y;
+					continue;
+				}
+				// else
+				if ( charCode == Character::Code::idTab )
+				{
+					drawPos.x += fontSize.x * 4.0f;
+					continue;
+				}
+				// else
+				if ( charCode == Character::Code::idSpace )
+				{
+					drawPos.x += fontSize.x;
+					continue;
+				}
+				// else
+
+				const auto &data = characters[charCode];
+				if ( data.pageNo < 0 || scast<int>( textures.size() ) <= data.pageNo ) { continue; }
+				// else
+
+				const auto &usingTexture = textures[data.pageNo];
+				elements[i].handle	= usingTexture.handle;
+				elements[i].pos		= drawPos + data.offset;
+				elements[i].size	= data.uvPartSize;
+				elements[i].uvPos	= data.uvMin;
+				elements[i].uvSize	= data.uvMax - data.uvMin;
+				// Donya::Sprite::DrawXX requires uv parameter as texture space
+				elements[i].uvPos.x		*= usingTexture.wholeSize.x;
+				elements[i].uvPos.y		*= usingTexture.wholeSize.y;
+				elements[i].uvSize.x	*= usingTexture.wholeSize.x;
+				elements[i].uvSize.y	*= usingTexture.wholeSize.y;
+
+				drawPos.x += data.advance;
+			}
+
+			// Sort for sprite batching
+			auto AscendingOrder = []( const Instance &lhs, const Instance &rhs )
+			{
+				return lhs.handle < rhs.handle;
+			};
+			std::sort( elements.begin(), elements.end(), AscendingOrder );
+
+			// Draw
+			for ( const auto &it : elements )
+			{
+				Donya::Sprite::DrawGeneral
+				(
+					it.handle,
+					it.pos.x,		it.pos.y,
+					it.size.x,		it.size.y,
+					it.uvPos.x,		it.uvPos.y,
+					it.uvSize.x,	it.uvSize.y,
+					0.0f, Donya::Sprite::Origin::LEFT_TOP,
+					color.w, color.x, color.y, color.z
+				);
+			}
+
+			Donya::Sprite::Flush();
+		}
+		void Renderer::Draw( const std::wstring &string,	const Donya::Vector2 &ssPos, const Donya::Vector4 &color ) const
+		{
+			Draw( string.c_str(), ssPos, color );
+		}
+	}
+}
---
+++ Platform/Code/Donya/Font.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <array>
+#include <limits>	// Use std::numeric_limits
+#include <string>
+#include <vector>
+
+#undef max
+#undef min
+#include <cereal/types/array.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
+
+#include "Serializer.h"
+#include "Vector.h"
+
+namespace Donya
+{
+	/// <summary>
+	/// Use UTF-16
+	/// </summary>
+	namespace Font
+	{
+		using CharType = unsigned short; // Must be 2 byte for UTF-16
+
+		/// <summary>
+		/// Data per character
+		/// </summary>
+		struct Character
+		{
+			struct Code
+			{
+				static constexpr CharType idNull	= 0;
+				static constexpr CharType idEnd		= 0xFFFF; // End of code
+				static constexpr CharType idReturn	= 0xFFFE; // Line Feed
+				static constexpr CharType idTab		= 0xFFFD; // Horizontal Tab
+				static constexpr CharType idSpace	= 0xFFFC;
+			};
+		public:
+			Donya::Vector2	uvMin		{ 0.0f, 0.0f };	// Left-Top origin in texture space
+			Donya::Vector2	uvMax		{ 1.0f, 1.0f };	// Left-Top origin in texture space
+			Donya::Vector2	uvPartSize	{ 1.0f, 1.0f };	// Whole part size of character in texture space
+			Donya::Vector2	offset		{ 0.0f, 0.0f };	// Offset from Left-Top
+			float			advance		= 0.0f;			// Advancing distance after drawing this character
+			int				pageNo		= -1;			// Page number for multiple font textures, 0-based
+		private:
+			friend class cereal::access;
+			template<class Archive>
+			void serialize( Archive &archive, std::uint32_t version )
+			{
+				archive
+				(
+					CEREAL_NVP( uvMin		),
+					CEREAL_NVP( uvMax		),
+					CEREAL_NVP( uvPartSize	),
+					CEREAL_NVP( offset		),
+					CEREAL_NVP( advance		),
+					CEREAL_NVP( pageNo		)
+				);
+
+				if ( 1 <= version )
+				{
+					// archive( CEREAL_NVP( x ) );
+				}
+			}
+		};
+
+		/// <summary>
+		/// Store ".fnt" file data
+		/// </summary>
+		class Holder
+		{
+		private:
+			int									characterCount	= 256;	// Supporting character count
+			Donya::Vector2						fontSize		{ 32.0f, 32.0f };
+
+			// The '/' terminated file directory.
+			// The file name that contains the extension.
+			std::wstring						textureDirectory;		// The directory of textures that ended by '/'(e.g. "Foo/Base/Font/")
+			std::vector<std::wstring>			textureNames;			// The texture file names with extension. The directory of these textures is the same.
+
+			static constexpr CharType twoByteLimit = std::numeric_limits<CharType>::max(); // 0xFFFF
+			std::vector<Character>				characters;
+			std::array<CharType, twoByteLimit>	characterIndices;		// Store the index of the characters. You can access to character info as: characters[characterIndices[static_cast<CharType>(L'‚ ')]]
+		private:
+			friend class cereal::access;
+			template<class Archive>
+			void serialize( Archive &archive, std::uint32_t version )
+			{
+				archive
+				(
+					CEREAL_NVP( characterCount		),
+					CEREAL_NVP( fontSize			),
+					CEREAL_NVP( textureDirectory	),
+					CEREAL_NVP( textureNames		),
+					CEREAL_NVP( characters			),
+					CEREAL_NVP( characterIndices	)
+				);
+
+				if ( 1 <= version )
+				{
+					// archive( CEREAL_NVP( x ) );
+				}
+			}
+			static constexpr const char *serializeObjectName = "FontHolder";
+		public:
+			/// <summary>
+			/// It returns false if failed. It accepts only .fnt file.
+			/// </summary>
+			bool LoadFntFile( const std::wstring &fontFilePath );
+			/// <summary>
+			/// It returns false if failed. It accepts the only file that serialized by Holder.
+			/// </summary>
+			bool LoadByCereal( const std::string &filePath );
+			bool SaveByCereal( const std::string &filePath ) const;
+		public:
+			/// <summary>
+			/// Please set a directory that ended by '/'(e.g. "Foo/Base/Font/")
+			/// </summary>
+			void SetTextureDirectory( const std::wstring &directory );
+		public:
+			const Donya::Vector2						&GetFontSize()			const { return fontSize;			}
+			const std::wstring							&GetTextureDirectory()	const { return textureDirectory;	}
+			const std::vector<std::wstring>				&GetTextureNames()		const { return textureNames;		}
+			const std::vector<Character>				&GetCharacters()		const { return characters;			}
+			const std::array<CharType, twoByteLimit>	&GetCharacterIndices()	const { return characterIndices;	}
+		};
+
+		/// <summary>
+		/// Draw a string with Holder's data
+		/// </summary>
+		class Renderer
+		{
+		private:
+			struct Texture
+			{
+				size_t			handle = NULL;
+				Donya::Vector2	wholeSize;
+			};
+		private:
+			Holder					info;
+			std::vector<Texture>	textures; // size() == info.texturenames.size()
+		public:
+			/// <summary>
+			/// Initialize the font data and create textures.<para></para>
+			/// "maxInstancesCount" is upper-limit of batching of sprite, and relate in memory usage.<para></para>
+			/// It returns false if failed.
+			/// </summary>
+			bool Init( const Holder &fontData, size_t maxInstanceCount = 512U );
+		public:
+			void Draw( const wchar_t *string,		const Donya::Vector2 &ssPosLeftTop, const Donya::Vector4 &blendColor = { 1.0f, 1.0f, 1.0f, 1.0f } ) const;
+			void Draw( const std::wstring &string,	const Donya::Vector2 &ssPosLeftTop, const Donya::Vector4 &blendColor = { 1.0f, 1.0f, 1.0f, 1.0f } ) const;
+		};
+	}
+}
+CEREAL_CLASS_VERSION( Donya::Font::Character,	0 )
+CEREAL_CLASS_VERSION( Donya::Font::Holder,		0 )
