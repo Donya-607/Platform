9740f29f3012944dad70bc83e5ee870b2b48aeff
Gunji <interactproxy.exe@gmail.com>
2020-12-06 14:42:36 +0900

    シーンの遷移先を本来のものにもどした

--- Platform/Code/SceneResult.cpp
+++ Platform/Code/SceneResult.cpp
@@ -1732,8 +1732,8 @@ Scene::Result SceneResult::ReturnResult()
 		Scene::Result change{};
 		change.AddRequest( Scene::Request::ADD_SCENE, Scene::Request::REMOVE_ME );
 	#if DEBUG_MODE
-		// change.sceneType = Scene::Type::Title;
-		change.sceneType = Scene::Type::Result;
+		change.sceneType = Scene::Type::Title;
+		// change.sceneType = Scene::Type::Result;
 	#else
 		change.sceneType = Scene::Type::Title;
 	#endif // DEBUG_MODE
