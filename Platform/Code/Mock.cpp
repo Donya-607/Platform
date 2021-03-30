bca3a498b3f398b8355b27ed8db49aeee2401a61
Donya <udongyouza3124@gmail.com>
2021-03-30 14:46:17 +0900

    ステートマシンクラスの設計・使用方法を模索している

--- Platform/Code/Mock.cpp
+++ Platform/Code/Mock.cpp
@@ -1,42 +1,100 @@
 #include "StateMachine.h"
 
-class Foo
+class Owner
 {
 public:
-	using StateBaseT = IStateMachine<Foo>;
+	using StateBaseT = StateMachine::IState<Owner>;
 public:
 	int hp = 0;
 	int max = 0;
 	std::unique_ptr<StateBaseT> pBase;
 public:
+	void Init();
+public:
+	void UpdateState()
+	{
+		if ( !pBase ) { return; }
+		// else
+
+		StateMachine::ChangeIfNeeded<Owner>( &pBase, *this );
+	}
 };
 
-class Hoge : Foo::StateBaseT
+
+using namespace StateMachine;
+#include "Donya/Template.h"
+
+class Idle : IState<Owner>
 {
-	void Init( Foo &instance ) override
+	void Init( Owner &instance ) override
 	{
 		instance.hp = 1;
 		instance.max = 2;
 	}
-	void Uninit( Foo &instance ) override
+	void Uninit( Owner &instance ) override
 	{
 
 	}
-	void Update( Foo &instance, float elapsedTime ) override
+	void Update( Owner &instance, float elapsedTime ) override
 	{
 
 	}
-	void PhysicUpdate( Foo &instance, float elapsedTime ) override
+	void PhysicUpdate( Owner &instance, float elapsedTime ) override
 	{
 
 	}
-	void Draw( Foo &instance ) override
+	void Draw( Owner &instance ) override
 	{
 
 	}
 public:
-	std::unique_ptr<Foo::StateBaseT> MakeNextStateOrNull( Foo &instance ) override
+	std::unique_ptr<IState<Owner>> MakeNextStateOrNull( Owner &instance ) override
+	{
+		return nullptr;
+	}
+};
+class Attack : IState<Owner>
+{
+	void Init( Owner &instance ) override
+	{
+		instance.hp = 65535;
+		instance.max = 1001001001;
+	}
+	void Uninit( Owner &instance ) override
 	{
 
 	}
+	void Update( Owner &instance, float elapsedTime ) override
+	{
+
+	}
+	void PhysicUpdate( Owner &instance, float elapsedTime ) override
+	{
+
+	}
+	void Draw( Owner &instance ) override
+	{
+
+	}
+public:
+	std::unique_ptr<IState<Owner>> MakeNextStateOrNull( Owner &instance ) override
+	{
+		// Donya::TypeDetective<Idle> hogeT;
+		// return std::make_unique<Owner>();
+		// return std::make_unique<Idle>();
+		
+		return std::make_unique<IState<Owner>>();
+		return std::make_unique<Idle>();
+
+		// return std::make_unique<Idle>();
+		return nullptr;
+	}
 };
+
+
+void Owner::Init()
+{
+	UpdateState();
+
+	pBase->Init( *this );
+}
--- Platform/Code/StateMachine.h
+++ Platform/Code/StateMachine.h
@@ -2,46 +2,47 @@
 
 #include <memory>
 
-template<class StateOwnerT>
-class IStateMachine
+namespace StateMachine
 {
+	template<class StateOwnerT>
+	class IState
+	{
+	public:
+		IState() = default;
+		virtual ~IState() = default;
+	public:
+		virtual void Init( StateOwnerT &ownerInstance ) = 0;
+		/// <summary>
+		/// It will be called when after MakeNextStateOrNull()
+		/// </summary>
+		virtual void Uninit( StateOwnerT &ownerInstance ) = 0;
+		virtual void Update( StateOwnerT &ownerInstance, float elapsedTime ) = 0;
+		virtual void PhysicUpdate( StateOwnerT &ownerInstance, float elapsedTime ) = 0;
+		virtual void Draw( StateOwnerT &ownerInstance ) = 0;
+	public:
+		/// <summary>
+		/// Returns nullptr means: do not change now
+		/// </summary>
+		virtual std::unique_ptr<IState<StateOwnerT>> MakeNextStateOrNull( StateOwnerT &ownerInstance ) = 0;
+	};
 
-public:
-	IStateMachine() = default;
-	virtual ~IStateMachine() = default;
-public:
-	virtual void Init( StateOwnerT &ownerInstance ) = 0;
-	/// <summary>
-	/// It will be called when after MakeNextStateOrNull()
-	/// </summary>
-	virtual void Uninit( StateOwnerT &ownerInstance ) = 0;
-	virtual void Update( StateOwnerT &ownerInstance, float elapsedTime ) = 0;
-	virtual void PhysicUpdate( StateOwnerT &ownerInstance, float elapsedTime ) = 0;
-	virtual void Draw( StateOwnerT &ownerInstance ) = 0;
-public:
-	/// <summary>
-	/// Returns nullptr means: do not change now
-	/// </summary>
-	virtual std::unique_ptr<IStateMachine<StateOwnerT>> MakeNextStateOrNull( StateOwnerT &ownerInstance ) = 0;
-};
+	template<class StateOwnerT>
+	void ChangeIfNeeded( std::unique_ptr<IState<StateOwnerT>> *pOperatee, StateOwnerT &ownerInstance )
+	{
+		if ( !pOperatee ) { return; }
+		// else
 
-template<class StateOwnerT>
-void ChangeStateTo( std::unique_ptr<IStateMachine<StateOwnerT>> *pOperatee, StateOwnerT &ownerInstance )
-{
-	if ( !pOperatee ) { return; }
-	// else
+		using Ptr = std::unique_ptr<IState<StateOwnerT>>;
+		Ptr &operatee = *pOperatee;
 
-	using Ptr = std::unique_ptr<IStateMachine<StateOwnerT>>;
-	Ptr &operatee = *pOperatee;
+		Ptr nextState = operatee->MakeNextStateOrNull( ownerInstance );
+		if ( !nextState ) { return; }
+		// else
 
-	Ptr nextState = operatee->GetNextStateOrNull( ownerInstance );
-	if ( !nextState ) { return; }
-	// else
+		operatee->Uninit( ownerInstance );
+		operatee.reset();
 
-	operatee->Uninit( ownerInstance );
-	operatee.reset();
-
-	operatee = std::move( nextState );
-	operatee->Init( ownerInstance );
+		operatee = std::move( nextState );
+		operatee->Init( ownerInstance );
+	}
 }
-
